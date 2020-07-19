f8285f801cc04e72fd1f1aa0bf687e3a61db3ba5
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-07-19 09:51:12 +0900

    update LinearAlgebra.php

--- src/LinearAlgebra.php
+++ src/LinearAlgebra.php
@@ -1312,7 +1312,8 @@ class LinearAlgebra
         $padding = ($padding) ? true:false;
         $channels_first = ($channels_first) ? true:false;
         $cols_channels_first = ($cols_channels_first) ? true:false;
-        if($cols==null) {
+        if($cols==null) 
+            echo "padding=";var_dump($padding);
             if($padding) {
                 $out_h = $in_h;
                 $out_w = $in_w;
