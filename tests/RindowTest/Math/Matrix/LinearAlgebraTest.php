04308607fecef284f930e3cb14dd9bb3002a70be
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-02 06:20:49 +0900

    update LinearAlgebraTest.php

--- tests/RindowTest/Math/Matrix/LinearAlgebraTest.php
+++ tests/RindowTest/Math/Matrix/LinearAlgebraTest.php
@@ -1793,8 +1793,8 @@ class Test extends TestCase
         $this->assertNotEquals(
             $x->toArray(),
             $y->toArray());
-        $this->assertLessThanOrEqual(1,max($x->toArray()))
-        $this->assertGreaterThanOrEqual(-1,min($x->toArray()))
+        $this->assertLessThanOrEqual(1,max($x->toArray()));
+        $this->assertGreaterThanOrEqual(-1,min($x->toArray()));
 
         $x = $mo->la()->randomUniform(
             $shape=[20,30],
@@ -1812,7 +1812,7 @@ class Test extends TestCase
         $this->assertNotEquals(
             $x->toArray(),
             $y->toArray());
-        $this->assertLessThanOrEqual(1,max($x->toArray()))
-        $this->assertGreaterThanOrEqual(-1,min($x->toArray()))
+        $this->assertLessThanOrEqual(1,max($x->toArray()));
+        $this->assertGreaterThanOrEqual(-1,min($x->toArray()));
     }
 }
