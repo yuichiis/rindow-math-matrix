e6f87ca0a5fd78c336a07b53e4c72f273ee25971
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-20 18:22:20 +0900

    update PhpMath.php

--- src/PhpMath.php
+++ src/PhpMath.php
@@ -1877,7 +1877,7 @@ class PhpMath
         for($i=0;$i<$sizeAxis0;$i++) {
             for($j=0;$j<$sizeAxis1;$j++){
                 $pa = ($i+$startAxis0)*$n+($j+$startAxis1)*$k+$offsetA;
-                $py = $i*$n+$j*$k+$offsetY;
+                $py = $i*$n*$k+$j*$k+$offsetY;
                 $this->rindow_openblas_math_copy($k,$A,$pa,$incA,$Y,$py,$incY);
             }
         }
