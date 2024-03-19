a40380007f565e956e367364412f349792dd584f
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-03-19 11:07:43 +0900

    test

--- tests/RindowTest/Math/Matrix/LinearAlgebraTest.php
+++ tests/RindowTest/Math/Matrix/LinearAlgebraTest.php
@@ -7749,7 +7749,6 @@ class LinearAlgebraTest extends TestCase
                 'dilation_w' => 2,
                 'cols_channels_first' => null,
             ]],
-/*
             'normal channels_first' => [[
                 'batches' => 2,
                 'im_d' => 8,
@@ -8130,6 +8129,7 @@ class LinearAlgebraTest extends TestCase
                 'dilation_w' => 2,
                 'cols_channels_first' => null,
             ]],
+/*
             'normal cols_channels_first' => [[
                 'batches' => 2,
                 'im_d' => 8,
