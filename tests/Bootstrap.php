bee0e76e04c5a5ba5857d6d227b4d7574fbd5d33
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-03-27 11:52:52 +0900

    remove debug

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -9,7 +9,6 @@ if(file_exists(__DIR__.'/../vendor/autoload.php')) {
     $loader = require_once __DIR__.'/init_autoloader.php';
 }
 if(file_exists(__DIR__.'/../addpack/vendor/autoload.php')) {
-    echo "Addpack found!!\n";
     $addpack = __DIR__.'/../addpack/vendor/rindow/';
     $loader->addPsr4('Rindow\\Math\\Matrix\\Drivers\\MatlibFFI\\',$addpack.'rindow-math-matrix-matlibffi/src');
     $loader->addPsr4('Rindow\\Math\\Buffer\\FFI\\',$addpack.'rindow-math-buffer-ffi/src');
@@ -17,8 +16,6 @@ if(file_exists(__DIR__.'/../addpack/vendor/autoload.php')) {
     $loader->addPsr4('Rindow\\Matlib\\FFI\\',$addpack.'rindow-matlib-ffi/src');
     $loader->addPsr4('Rindow\\OpenCL\\FFI\\',$addpack.'rindow-opencl-ffi/src');
     $loader->addPsr4('Rindow\\CLBlast\\FFI\\',$addpack.'rindow-clblast-ffi/src');
-    $a = new Rindow\Math\Matrix\Drivers\MatlibFFI\MatlibFFI();
-    echo get_class($a)."\n";
 }
 #if(!class_exists('PHPUnit\Framework\TestCase')) {
 #    include __DIR__.'/travis/patch55.php';
