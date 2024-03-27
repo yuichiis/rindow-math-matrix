e916e857d78944b9a1a3d9613b35090e85fd2dfa
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-03-27 11:43:18 +0900

    ffi

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -11,6 +11,8 @@ if(file_exists(__DIR__.'/../vendor/autoload.php')) {
 if(file_exists(__DIR__.'/../addpack/vendor/autoload.php')) {
     echo "Addpack found!!\n";
     $loader->addPsr4('Rindow\\Math\\Matrix\\Drivers\\MatlibFFI\\',__DIR__.'/../addpack/vendor/rindow/rindow-math-matrix-matlibffi/src');
+    $a = new Rindow\Math\Matrix\Drivers\MatlibFFI\MatlibFFI();
+    echo get_class($a)."\n";
 }
 #if(!class_exists('PHPUnit\Framework\TestCase')) {
 #    include __DIR__.'/travis/patch55.php';
