0e7076ee4c07f1fdea4f397947e81770f3aa39d7
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2025-04-12 22:21:49 +0900

    test

--- tests/RindowTest/Math/Matrix/LinearAlgebra2Test.php
+++ tests/RindowTest/Math/Matrix/LinearAlgebra2Test.php
@@ -135,6 +135,10 @@ class LinearAlgebra2Test extends TestCase
     public function testSvdSmallU()
     {
         $mo = $this->newMatrixOperator();
+        if((PHP_OS==='Darwin') && $mo->isAdvanced()) {
+            $this->markTestSkipped('macOS avoid hang up');
+            return;
+        }
         $la = $this->newLA($mo);
         $a = $la->array([
             [ 8.79,  9.93,  9.83,  5.45,  3.16,],
