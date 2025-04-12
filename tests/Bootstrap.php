d18e440ad091c6e2fad65a4777b1dca954acac27
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2025-04-12 16:03:39 +0900

    test

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -4,10 +4,10 @@ ini_set('short_open_tag', '1');
 date_default_timezone_set('UTC');
 #ini_set('short_open_tag',true);
 if(file_exists(__DIR__.'/../vendor/autoload.php')) {
-    $loader = require_once __DIR__.'/../vendor/autoload.php';
+    $loader = require __DIR__.'/../vendor/autoload.php';
     echo "autoload!!\n";
 } else {
-    $loader = require_once __DIR__.'/init_autoloader.php';
+    $loader = require __DIR__.'/init_autoloader.php';
     echo "init_autoload!!\n";
 }
 $addpack = getenv('ADD_PACK');
