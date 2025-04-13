43aa460808d5e6c9bca864c786bc61a330126bf6
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2025-04-13 17:37:23 +0900

    test

---
+++ tests/CheckAdvanced.php
@@ -0,0 +1,14 @@
+<?php
+namespace RindowTest\Math\Matrix\CheckAdvanced;
+
+use PHPUnit\Framework\TestCase;
+
+class CheckAdvanced extends TestCase
+{
+    public function testAdvanced()
+    {
+        $mo = new \Rindow\Math\Matrix\MatrixOperator();
+        $this->assertTrue($mo->isAdvanced());
+        $this->assertTrue(false)
+    }
+}
